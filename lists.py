9316b2c6175be32660e24f4659fc66d17861274d
Luhan Salimena <luhan@beepayapp.com.br>
2024-03-30 18:02:12 -0300

    Add code to calculate sum of even numbers in the list

--- lists.py
+++ lists.py
@@ -3,3 +3,10 @@ oneToTen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 names = ['Luhan', 'Natália', 'Roberto', 'Luane']
 
 years = [2003,2024]
+
+sum = 0
+for number in oneToTen:
+    if(number % 2 == 0):
+        sum += number
+
+print(sum)
